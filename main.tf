1039e52dfbad13186049c042d5667d97e6ddf3b5
rajeshanagani <rajeshanagani@outlook.com>
2024-08-13 23:14:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "three" {
-  count         =  3
+  count         =  1
   ami           = "ami-00b8917ae86a424c9"
   instance_type = "t2.medium"
   key_name      = "newtestkey"
