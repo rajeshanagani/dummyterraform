fba1f9952006b625f6e54b8b7025fdc2386cf140
rajeshanagani <rajeshanagani@outlook.com>
2024-08-13 22:54:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,12 +6,8 @@ resource "aws_instance" "three" {
   count         =  3
   ami           = "ami-00b8917ae86a424c9"
   instance_type = "t2.medium"
-  key_name      = "mykeypair9am"
+  key_name      = "newtestkey"
   tags = {
-    Name = "mumbai-server"
+    Name = "test-server"
   }
-}
-
-resource "aws_s3_bucket" "abcd" {
-bucket = "devopsherahamshaik0099889977hcp"
 }
